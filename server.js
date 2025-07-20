8df10042938012d741d1a483ab67306a4762f8d1
Haqqnuru <haqqnuru@gmail.com>
2025-07-20 18:35:26 +0000

    added DATABASE_URL to server.js

--- server.js
+++ server.js
@@ -9,15 +9,14 @@ const knex = require('knex')
 
 // call the database
 const db = knex({
-    client: 'pg',
-    connection: {
-      host: '127.0.0.1',
-      port: 5432,
-      user: 'postgres',
-      password: '1234',
-      database: 'facebrain',
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
     },
-  });
+  },
+});
 
 //this creates the “connection” to Clarifai’s services
 const stub = ClarifaiStub.grpc();
